fe3a19a9f89ddcf0fe1c1a7f7dc37a7de0999fe8
Julio C. Jesus <143628023+juliocesarcj@users.noreply.github.com>
2025-10-18 13:23:51 -0300

    Update header file inclusion in trabalho1.c

--- trabalho1/trabalho1.c
+++ trabalho1/trabalho1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "JulioJesus-20232160030-T1.h" // Substitua pelo seu arquivo de header renomeado
+#include "trabalho1.h" // Substitua pelo seu arquivo de header renomeado
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
