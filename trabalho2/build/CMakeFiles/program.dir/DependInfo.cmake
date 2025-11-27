c4de2b580dea055b27217fd0046947db2fb0b9a3
julio <juliojesus318@gmail.com>
2025-11-27 03:36:03 -0300

    init T2

---
+++ trabalho2/build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kuro/Documentos/Trabalhos/MEU_T2/mainTeste.c" "CMakeFiles/program.dir/mainTeste.c.o" "gcc" "CMakeFiles/program.dir/mainTeste.c.o.d"
+  "/home/kuro/Documentos/Trabalhos/MEU_T2/t2.c" "CMakeFiles/program.dir/t2.c.o" "gcc" "CMakeFiles/program.dir/t2.c.o.d"
+  "" "program" "gcc" "CMakeFiles/program.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ trabalho2/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/link.d"
+  "CMakeFiles/program.dir/mainTeste.c.o"
+  "CMakeFiles/program.dir/mainTeste.c.o.d"
+  "CMakeFiles/program.dir/t2.c.o"
+  "CMakeFiles/program.dir/t2.c.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
